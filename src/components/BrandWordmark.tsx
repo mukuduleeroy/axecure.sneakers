7a44650fd1b2f00dc00b4808b4bcef97b23f5ddd
mukuduleeroy <leeroymukudu588@gmail.com>
2026-09-11 14:35:57 +0200

    added a wordmark

---
+++ src/components/BrandWordmark.tsx
@@ -0,0 +1,7 @@
+export function BrandWordmark() {
+  return (
+    <span aria-label="AxeCure" className="brand-wordmark">
+      <img alt="" aria-hidden="true" src="/brand mark logo.png" />
+    </span>
+  );
+}
